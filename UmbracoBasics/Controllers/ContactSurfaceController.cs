e399888933c48c4db619104c5f0895aa8271999b
Amol <kumaramol44@yahoo.co.in>
2019-03-13 15:24:10 +0100

    This is my first commit for testing project

---
+++ UmbracoBasics/Controllers/ContactSurfaceController.cs
@@ -0,0 +1,38 @@
+﻿using Umbraco.Web.Mvc;
+using System.Web.Mvc;
+using System;
+using UmbracoBasics.Models;
+using System.Net.Mail;
+
+namespace UmbracoBasics.Controllers
+{
+    public class ContactSurfaceController : SurfaceController
+    {
+        public const string PARTIAL_VIEW_FOLDER = "~/Views/Partials/Contact/";
+        public ActionResult index()
+        {
+            return View();
+        }
+        public ActionResult RenderForm()
+        {
+            return PartialView(PARTIAL_VIEW_FOLDER + "_Contact.cshtml");
+        }
+        public ActionResult SubmitForm(ContactModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                SendEmail(model);
+                return RedirectToCurrentUmbracoPage();
+            }
+            return CurrentUmbracoPage();
+        }
+        private void SendEmail(ContactModel model)
+        {
+            MailMessage message = new MailMessage(model.EmailAddress, "amolkumar.bambarde@transfergalaxy.com");
+            message.Subject = "Test Mail";
+            message.Body = "Test";
+            SmtpClient client = new SmtpClient("127.0.0.1", 25);
+            client.Send(message);
+        }
+    }
+}
---
+++ UmbracoBasics/Models/ContactModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UmbracoBasics.Models
+{
+    public class ContactModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
